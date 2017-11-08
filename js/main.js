2a826ede4c0e2f648ce4b1da17b0614fa8827444
Suwart, Jan <jan.suwart@aperto.com>
2017-11-08 11:49:19 +0100

    Add Boilerplate html, css and javascript

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import Timepicker from './timepicker-es6';
+
+console.log('Timepicker', Timepicker(null, null));
---
+++ js/timepicker-es6.js
@@ -0,0 +1,22 @@
+/**
+ * Timepicker input field extension dependent on jQuery or VeamsQuery.
+ *
+ * @module Timepicker
+ * @version 0.1.0
+ *
+ * @author Jan Suwart
+*/
+class TimepickerObject {
+
+	constructor(options, context) {
+
+	}
+}
+
+function Timepicker(options, context) {
+	return new TimepickerObject(options, context);
+}
+
+Timepicker.version = '0.1.0';
+
+export default Timepicker;
